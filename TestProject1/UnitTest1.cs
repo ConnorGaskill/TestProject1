dc5cb8253d0b43240c5fae391438401125f8565f
Connor Gaskill <gaskillconnor@gmail.com>
2025-04-13 09:16:51 -0700

    Added three tests and passed them.

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,32 @@
-﻿namespace TestProject1
+﻿using Xunit;
+
+namespace TestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Positive_X_Positive_is_Positive()
         {
+            int result = Multiply(1, 2);
+            Assert.Equal(2, result);
 
+        }
+        [Fact]
+        public void Positive_X_Negative_is_Negative()
+        {
+            int result = Multiply(-1, 2);
+            Assert.Equal(-2, result);
+        }
+        [Fact]
+        public void Negative_X_Negative_is_Positive()
+        {
+            int result = Multiply(-1, -2);
+            Assert.Equal(2, result);
+        }
+
+        private int Multiply(int a,  int b)
+        {
+            return a*b;
         }
     }
 }
